1bca098a5da28a8b9799b71168692959ac56a291
sangbo-seo <sangbo.seo@gmail.com>
2024-08-11 06:09:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,31 @@
 import streamlit as st
+from konlpy.tag import *
 
-import socket
-import requests
-import re
-import random
-import time
-#import pyperclip
-#from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.alert import Alert
-from datetime import datetime
+# 제목 설정
+st.title("한글/영어 구분 및 글자수 세기 애플리케이션")
 
-def scrape_website(url):
-    # Webdriver 설정
-    # Chrome WebDriver 경로 설정
-    customService = Service()  # ChromeDriver의 경로로 변경하세요
-    customOptions = Options()
-  #chrome_options.add_argument("--headless")  # 브라우저 창을 띄우지 않고 실행
-    customOptions.add_argument("--window-size=600,800")  # 브라우저 창을 띄우지 않고 실행
-  # 브라우저 열기
- 
-    driver = webdriver.Chrome(service=customService, options=customOptions)  
-    driver.implicitly_wait(30)
-    #site_url = 'https://nid.naver.com/nidlogin.login?mode=form&url=https://m.naver.com/'
-    
-    # 웹 페이지 접근
-    driver.get(url)
-    
-    # 여기에 스크래핑 로직 추가
-    title = driver.title
-    time.sleep(20)
-    driver.quit()
-    return title
+# 사용자로부터 텍스트 입력받기
+user_input = st.text_area("텍스트를 입력하세요:")
 
-# Streamlit 앱
-st.title('Visit your place')
+# 공백 포함 글자 수
+total_char_count = len(user_input)
 
-url = st.text_input('Enter a URL to visit:')
-if st.button('Visit!'):
-    if url:
-        result = scrape_website(url)
-        st.write(f"Title of the page: {result}")
-    else:
-        st.write("Please enter a URL")
+# 공백 제외 글자 수
+char_count_no_spaces = len(user_input.replace(" ", ""))
 
+# 한글과 영어 구분
+korean_text = ''.join([char for char in user_input if '가' <= char <= '힣'])
+english_text = ''.join([char for char in user_input if 'a' <= char.lower() <= 'z'])
 
-        
+# 한글 형태소 분석
+#okt = Okt()
+#hannanum = Hannanum()
+#korean_morphs =  hannanum.nouns(korean_text)
+#korean_morphs = okt.morphs(korean_text)
+
+# 결과 출력
+st.write(f"입력한 텍스트의 총 글자 수 (공백 포함): {total_char_count}자")
+st.write(f"입력한 텍스트의 글자 수 (공백 제외): {char_count_no_spaces}자")
+st.write(f"한글 글자 수: {len(korean_text)}자")
+st.write(f"영어 글자 수: {len(english_text)}자")
+#st.write(f"한글 형태소 분석 결과: {korean_morphs}")
