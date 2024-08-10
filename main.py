b5a0ef7e1477d1fda067f269cfca3f57c7d0cdfa
sangbo-seo <sangbo.seo@gmail.com>
2024-08-10 14:59:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
 import streamlit as st
+
+import socket
 import requests
+import re
+from tkinter import messagebox
+import tkinter as tk
+from tkinter import Entry, Label, Tk
+import random
+import time
+#import pyperclip
+#from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.alert import Alert
+from datetime import datetime
 
-# Streamlit 애플리케이션 제목
-st.title('웹사이트 접속 예제')
-
-
-# 사용자로부터 URL 입력받기
-url = st.text_input('열고 싶은 URL을 입력하세요:', 'https://www.example.com')
-
-# 버튼 클릭 시 JavaScript로 새 창 열기
-if st.button('새 창으로 열기'):
-    js_code = f"""
-    <script type="text/javascript">
-        window.open("{url}");
-    </script>
-    """
-    st.components.v1.html(js_code)
-
-
-# 사용자로부터 URL 입력받기
-url = st.text_input('접속할 URL을 입력하세요:', 'https://m.naver.com')
-
-# 접속 버튼
-if st.button('접속하기'):
-    # URL 접속 시도
-    try:
-        response = requests.get(url)
-        
-        # 응답 성공 시, 상태 코드와 콘텐츠를 출력
-        if response.status_code == 200:
-            st.success(f"성공적으로 {url}에 접속했습니다.")
-            st.write(f"응답 코드: {response.status_code}")
-            st.write(response.text[:1000])  # 응답 내용의 일부를 출력
-        else:
-            st.error(f"{url}에 접속하는 데 실패했습니다. 상태 코드: {response.status_code}")
+def scrape_website(url):
+    # Webdriver 설정
+    # Chrome WebDriver 경로 설정
+    customService = Service()  # ChromeDriver의 경로로 변경하세요
+    customOptions = Options()
+  #chrome_options.add_argument("--headless")  # 브라우저 창을 띄우지 않고 실행
+    customOptions.add_argument("--window-size=600,800")  # 브라우저 창을 띄우지 않고 실행
+  # 브라우저 열기
+ 
+    driver = webdriver.Chrome(service=customService, options=customOptions)  
+    driver.implicitly_wait(30)
+    #site_url = 'https://nid.naver.com/nidlogin.login?mode=form&url=https://m.naver.com/'
     
-    except requests.exceptions.RequestException as e:
-        # 예외 발생 시, 오류 메시지 출력
-        st.error(f"에러 발생: {e}")
+    # 웹 페이지 접근
+    driver.get(url)
+    
+    # 여기에 스크래핑 로직 추가
+    title = driver.title
+    time.sleep(20)
+    driver.quit()
+    return title
 
+# Streamlit 앱
+st.title('Visit your place')
+
+url = st.text_input('Enter a URL to visit:')
+if st.button('Visit!'):
+    if url:
+        result = scrape_website(url)
+        st.write(f"Title of the page: {result}")
+    else:
+        st.write("Please enter a URL")
+
+
+        
