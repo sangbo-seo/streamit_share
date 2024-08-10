7f4574e151574afdc4d8de5b6ae291eba383f37d
sangbo-seo <sangbo.seo@gmail.com>
2024-08-11 06:17:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ char_count_no_spaces = len(user_input.replace(" ", ""))
 korean_text = ''.join([char for char in user_input if '가' <= char <= '힣'])
 english_text = ''.join([char for char in user_input if 'a' <= char.lower() <= 'z'])
 
-# 결과 출력
-st.write(f"입력한 텍스트의 총 글자 수 (공백 포함): {total_char_count}자")
-st.write(f"입력한 텍스트의 글자 수 (공백 제외): {char_count_no_spaces}자")
-st.write(f"한글 글자 수: {len(korean_text)}자")
-st.write(f"영어 글자 수: {len(english_text)}자")
+
+if st.button('글자수 계산'):
+    # 결과 출력
+    st.write(f"입력한 텍스트의 총 글자 수 (공백 포함): {total_char_count}자")
+    st.write(f"입력한 텍스트의 글자 수 (공백 제외): {char_count_no_spaces}자")
+    st.write(f"한글 글자 수: {len(korean_text)}자")
+    st.write(f"영어 글자 수: {len(english_text)}자")
