4af8cc3e2775a269465d48c4eed006a96cda55a8
sangbo-seo <sangbo.seo@gmail.com>
2024-08-11 06:14:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from konlpy.tag import *
+
 
 # 제목 설정
 st.title("한글/영어 구분 및 글자수 세기 애플리케이션")
@@ -17,15 +17,8 @@ char_count_no_spaces = len(user_input.replace(" ", ""))
 korean_text = ''.join([char for char in user_input if '가' <= char <= '힣'])
 english_text = ''.join([char for char in user_input if 'a' <= char.lower() <= 'z'])
 
-# 한글 형태소 분석
-#okt = Okt()
-#hannanum = Hannanum()
-#korean_morphs =  hannanum.nouns(korean_text)
-#korean_morphs = okt.morphs(korean_text)
-
 # 결과 출력
 st.write(f"입력한 텍스트의 총 글자 수 (공백 포함): {total_char_count}자")
 st.write(f"입력한 텍스트의 글자 수 (공백 제외): {char_count_no_spaces}자")
 st.write(f"한글 글자 수: {len(korean_text)}자")
 st.write(f"영어 글자 수: {len(english_text)}자")
-#st.write(f"한글 형태소 분석 결과: {korean_morphs}")
